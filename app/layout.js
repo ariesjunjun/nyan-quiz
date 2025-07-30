37d44374c2cb20e08ec9d2397be5a8930fe61ab8
ariesjunjun <aoarashihazuki@gmail.com>
2025-07-30 14:11:02 +0900

    ボタンの位置

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export const metadata = {
-  title: "保護猫クイズ",
+  title: "保護猫譲渡大作戦 by ニャンズマーケット ",
   description: "保護猫について楽しく学べるクイズアプリ",
   icons: {
     icon: "/favicon.ico", // public直下のファイル
