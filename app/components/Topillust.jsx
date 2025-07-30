5e752d01b456c3eb2fcc4248a9d8f75f1b3940ce
ariesjunjun <aoarashihazuki@gmail.com>
2025-07-30 14:20:18 +0900

    画像をコンポーネントに

---
+++ app/components/Topillust.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Image from 'next/image'
+
+export const Topillust = () => {
+  return (
+          <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "2rem",
+        marginBottom: "2rem",
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+      }}
+    >
+      <div
+        style={{
+          width: "80vw",
+          maxWidth: "300px",
+          aspectRatio: "1 / 1", // 正方形にする
+          borderRadius: "50%",
+          backgroundColor: "white",
+          overflow: "hidden", // はみ出た部分を隠す
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          src="/hogoneko_top.jpg"
+          alt="クイズのメイン画像"
+          width={300}
+      height={300}
+          style={{
+            objectFit: "contain", // 切れずに全体表示
+            transform: "scale(0.75)", // ← ここで画像だけ小さくする
+          }}
+        />
+      </div>
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Quiz from "./components/Quiz";
 import Image from 'next/image';
 import FooterFixed from "./components/FooterFixed";
+import { Topillust } from "./components/Topillust";
 
 export default function Home() {
   return (
@@ -8,42 +9,7 @@ export default function Home() {
 
 <Quiz />
 
-      <div
-  style={{
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "2rem",
-    marginBottom: "2rem",
-    paddingLeft: "1rem",
-    paddingRight: "1rem",
-  }}
->
-  <div
-    style={{
-      width: "80vw",
-      maxWidth: "300px",
-      aspectRatio: "1 / 1", // 正方形にする
-      borderRadius: "50%",
-      backgroundColor: "white",
-      overflow: "hidden", // はみ出た部分を隠す
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <Image
-      src="/hogoneko_top.jpg"
-      alt="クイズのメイン画像"
-      width={300}
-  height={300}
-      style={{
-        objectFit: "contain", // 切れずに全体表示
-        transform: "scale(0.75)", // ← ここで画像だけ小さくする
-      }}
-    />
-  </div>
-</div>
+<Topillust/>
 
 
        {/* TOPページだけ固定フッター */}
